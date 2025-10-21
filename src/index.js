c6eab7ecd39a6c93c33fb80303677edae1ca58a6
angedev42 <angepro14@gmail.com>
2025-10-21 21:53:28 +0200

    activity

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './style.css';
 import { getWeatherData } from './api.js';
 import { display7DayForecast } from './weather.js';
-import { loadEvents } from './events';
 import { createMenu } from './menu';
+import { renderActivity } from "./renderactivity.js";
 
 createMenu(); // Ajoute le menu en haut de la page
 
@@ -12,4 +12,23 @@ getWeatherData().then(data => {
   }
 });
 
-injectContactForm();
+let cart = {};
+
+const activityContainer = document.getElementById("activityContainer");
+const searchInput = document.getElementById("searchInput");
+const categoryFilter = document.getElementById("categoryFilter");
+
+function renderCart() {
+  console.log("Cart updated:", cart);
+}
+
+searchInput.addEventListener("input", () => {
+  renderActivity({ cart, activityContainer, searchInput, categoryFilter, renderCart });
+});
+
+categoryFilter.addEventListener("change", () => {
+  renderActivity({ cart, activityContainer, searchInput, categoryFilter, renderCart });
+});
+
+// Premier affichage
+renderActivity({ cart, activityContainer, searchInput, categoryFilter, renderCart });
